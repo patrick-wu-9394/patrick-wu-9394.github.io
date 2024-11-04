4b81238434ccbc310375406e5e88f4c8fd2871eb
Patrick Wu <patrickcwu9394@gmail.com>
2024-11-03 21:31:11 -0800

    Added sidebar. Moved theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
-import ThemeToggleSwitch from "@/components/theme-toggle-switch";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <header className="p-4 text-blue-800 dark:text-blue-400">
-        This is my header
-      </header>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarTrigger />
 
-      <main className="flex-grow p-4 text-blue-800 dark:text-blue-400">
+      <main className="flex-grow p-4 text-blue-800 dark:text-red-400">
         This is my main
       </main>
-      <footer className="flex flex-wrap items-center justify-center p-4">
-        <div className="col-auto">
-          <ThemeToggleSwitch></ThemeToggleSwitch>
-        </div>
-      </footer>
-    </div>
+    </SidebarProvider>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,30 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
+  darkMode: ["selector"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
