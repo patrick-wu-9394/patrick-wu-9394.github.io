ac8b3ce9e3adf65bfca55963bfdb932acb6f0d51
patrick-wu-9394 <patrickcwu9394@gmail.com>
2025-10-31 08:15:43 -0700

    Empty project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex justify-center items-center min-h-screen">
+    <h1 class="text-4xl text-gray-800">Hello, World!</h1>
   </main>
 </template>
+<style scoped></style>
