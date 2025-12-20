f21b5b013cfb94a1b32116a1600ca6390fe23e23
Patrick Wu <38221997+patrick-wu-9394@users.noreply.github.com>
2025-12-20 18:31:28 -0500

    Add 404.html support for routing in GH Pages (#7)

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,11 @@ const app = createApp(App)
 
 app.use(router)
 
+// Handle GitHub Pages 404 redirect
+if (sessionStorage.redirect) {
+  const redirect = sessionStorage.redirect
+  delete sessionStorage.redirect
+  router.push(redirect)
+}
+
 app.mount('#app')
